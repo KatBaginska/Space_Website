65bbe87432c4aff025ff00395f96faf72a437982
KatBaginska <katarzyna.baginska11@icloud.com>
2024-02-22 21:58:05 +0000

    new page and dark mode

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function myFunction() {
-    window.location.href = "FirstPage.html";
+    window.location.href = "CheckMore.html";
 }
+
+function homePage() {
+    window.location.href = "index.html";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    const body = document.body;
+
+    // Check if dark mode is enabled
+    const isDarkMode = () => body.classList.contains('dark');
+
+    // Set dark mode based on user preference
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        body.classList.add('dark');
+    }
+
+    // Toggle dark mode
+    darkModeToggle.addEventListener('click', () => {
+        body.classList.toggle('dark');
+    });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
+  screens: {
+    'sm': '640px',
+    'md': '768px',
+    'ms': '900px',
+    'lg': '1024px',
+    'xl': '1280px',
+    '2xl': '1536px'
+  },
   content: [],
   theme: {
     extend: {
       fontFamily: { 
-        "Raleway": ['Raleway', 'sans-serif'] 
+        "Raleway": ['Raleway', 'sans-serif']
+      }
     },
   },
   plugins: [],
   }
-}
+
 
